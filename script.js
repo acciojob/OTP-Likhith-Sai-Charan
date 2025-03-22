f74748ede0146b73fa73cad3c44a324488f2b03e
Likhith Sai Charan Jupudi <143252814+Likhith-Sai-Charan@users.noreply.github.com>
2025-03-22 14:42:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,15 @@
-//your JS code here. If required.
 const codes=document.querySelectorAll('.code');
-
 codes[0].focus();
 
-codes.forEach((code,idx)=>{
-	code.addEventListener('input',(e)=>{
-		const val=e.target.value;
-		if(val.length>1){
-			e.target.value=val.charAt(0);
+codes.forEach((code, idx)=>{
+	code.addEventListener("input", (event)=>{
+		if(event.target.value.length === 1 && idx<codes.length-1){
+			codes[idx+1].focus()
 		}
-
-		if(val!=='' && idx<codes.length-1){
-			codes[idx+1].focus();
+	})
+	code.addEventListener("keydown", (event)=>{
+		if(event.key === 'Backspace' && !event.target.value && idx>0){
+			codes[idx-1].focus();
 		}
-	});
-
-	code.addEventListener('keydown',(e)=>{
-		if(e.key === 'Backspace'){
-			if(e.target.value==='' && idx>0){
-				codes[idx-1].focus();
-				codes[idx-1].value='';
-			}
-		}else if(e.key>='0' && e.key<='9'){}
-		else if(e.key!=='Tab'){
-			e.preventDefault();
-		}
-	});
-});
+	})
+})
